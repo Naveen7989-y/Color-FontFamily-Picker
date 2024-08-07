d9e2c2c98511360aa4e1acc6a8235c55a54773a8
Naveen7989-y <yerraguntanaveen7989@gmail.com>
2024-08-07 21:19:29 +0530

    FINALCOMMIT

--- script.js
+++ script.js
@@ -1,75 +1,79 @@
-const colors = ['red', 'green', 'blue', 'orange', 'purple', 'yellow', 'pink', 'brown', 'gray', 'black', 'white', 'cyan', 'magenta', 'lime', 'maroon', 'navy', 'olive', 'teal', 'violet', 'gold', 'silver', 'beige', 'coral', 'crimson', 'indigo', 'khaki', 'lavender', 'plum'];
-const fonts = ['Arial', 'Verdana', 'Times New Roman', 'Courier New', 'Georgia', 'Palatino', 'Garamond', 'Comic Sans MS', 'Trebuchet MS', 'Arial Black', 'Impact'];
+const colors = ['red', 'green', 'blue', 'orange', 'purple', 'yellow', 'pink', 'brown', 'gray', 'black', 'white', 'cyan', 'magenta', 'lime', 'maroon', 'navy', 'olive', 'teal', 'violet', 'gold', 'silver', 'beige', 'coral', 'crimson', 'indigo', 'khaki', 'lavender', 'plum','tomato','Gold','Burlywood','crimson'];
+const fonts = ['Arial', 'Verdana', 'Times New Roman', 'Courier New', 'Georgia', 'Palatino', 'Garamond', 'Comic Sans MS', 'Trebuchet MS', 'Arial Black', 'Impact','Cursive'];
 
-        const colorContainer = document.getElementById('colorContainer');
-        const fontSelector=document.getElementById('fontSelector');
-        const colorContainer2 =document.getElementById('colorContainer2');
-        const textcontainer=document.getElementById('textcontainer');
+const colorContainer = document.getElementById('colorContainer');
+const fontSelector = document.getElementById('fontSelector');
+const colorContainer2 = document.getElementById('colorContainer2');
+const textcontainer = document.getElementById('textcontainer');
 
 
-        colors.forEach((color,index) => {
-          const div=document.createElement('div');
-        //  const btn=document.createElement('button');
-          div.className='color-box';
-          
-          div.id='color-box';
-          colorContainer.appendChild(div);
-          div.style.backgroundColor=color;
-          div.onclick=() =>changefontcolor(color);
+colors.forEach((color, index) => {
+  const div = document.createElement('div');
+  //  const btn=document.createElement('button');
+  div.className = 'color-box';
 
-         // colorContainer.appendChild(btn);//
-          console.log(div);
-        });
-        
+  div.id = 'color-box';
+  colorContainer.appendChild(div);
+  div.style.backgroundColor = color;
+  div.onclick = () => changefontcolor(color);
 
-        function changefontcolor(color){
+  // colorContainer.appendChild(btn);//
+  console.log(div);
+});
 
-          textcontainer.style.color=color;
-        }
 
-        /*const colorbox=document.getElementById('color-box');
-        colorbox.addEventListener('click',() =>{
-          colorbox.style.backgroundColor+=textcontainer.style.color;
-            console.log(textcontainer.style);
-        });*/
+function changefontcolor(color) {
 
-        colors.forEach(color=>{
-            const div2=document.createElement('div');
-            div2.className='color-box2';
-            div2.style.backgroundColor=color;
-          
-            div2.onclick=()=>changebgcolor(color);
-            colorContainer2.appendChild(div2);
-        });
-        function changebgcolor(color){
-          textcontainer.style.backgroundColor=color;
-        }
+  textcontainer.style.color = color;
+}
 
-        fonts.forEach(font=>{
-            const div=document.createElement('div');
-            div.clasName='fontoption';
-            div.style.backgroundColor='#C9D6DF';
-            div.style.marginTop='-1.5px';
-            div.style.cursor='pointer';
-            div.textContent=font;
-            div.onclick=()=>changefont(font);
-            fontSelector.appendChild(div);
+/*const colorbox=document.getElementById('color-box');
+colorbox.addEventListener('click',() =>{
+  colorbox.style.backgroundColor+=textcontainer.style.color;
+    console.log(textcontainer.style);
+});*/
 
-        });
-        function changefont(font){
-          textcontainer.style.fontFamily=font;
-        }
-        fontSelector.style.border='10px solid black';
-        fontSelector.style.borderRadius='10px';
-        const btn1=document.getElementById('btn1');
-        
-        btn1.addEventListener('click', myfun);
+colors.forEach(color => {
+  const div2 = document.createElement('div');
+  div2.className = 'color-box2';
+  div2.style.backgroundColor = color;
 
-        function myfun(){
-          html2canvas(document.getElementById('textcontainer')).then(canvas=>{
-              const link=document.createElement('a');
-              link.href=canvas.toDataURL('image/png');
-              link.download='image.png';
-              link.click();
-          });
-        }
+  div2.onclick = () => changebgcolor(color);
+  colorContainer2.appendChild(div2);
+});
+function changebgcolor(color) {
+  textcontainer.style.backgroundColor = color;
+}
+
+fonts.forEach(font => {
+  const div = document.createElement('div');
+  div.clasName = 'fontoption';
+  div.id='font-option'
+  
+  div.style.marginTop = '-1.5px';
+  div.style.cursor = 'pointer';
+  div.textContent = font;
+  div.onclick = () => changefont(font);
+  fontSelector.appendChild(div);
+  console.log(div);
+
+});
+function changefont(font) {
+  textcontainer.style.fontFamily = font;
+}
+fontSelector.style.border = '1px solid black';
+fontSelector.style.paddingTop='8px'
+fontSelector.style.paddingLeft='14px'
+fontSelector.style.borderRadius = '10px';
+const btn1 = document.getElementById('btn1');
+
+btn1.addEventListener('click', myfun);
+
+function myfun() {
+  html2canvas(document.getElementById('textcontainer')).then(canvas => {
+    const link = document.createElement('a');
+    link.href = canvas.toDataURL('image/png');
+    link.download = 'image.png';
+    link.click();
+  });
+}
